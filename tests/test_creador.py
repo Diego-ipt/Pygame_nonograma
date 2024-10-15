a2ccb489ee67d2200071591251db9add4d72f231
JavierKDGN <javier.cadagan6@gmail.com>
2024-10-15 00:21:10 -0300

    tests para cambiar el tamano del creador

--- tests/test_creador.py
+++ tests/test_creador.py
@@ -12,3 +12,15 @@ def test_clickBoton():
     button = SizeButton(0,0,100,100,"Test",None)
     assert button.isClicked((50,50)) == True
     assert button.isClicked((150,150)) == False
+
+def test_decreaseGrid():
+    game = Game()
+    game.decreaseGrid()
+    assert game.grid_size == 9
+
+def test_increaseGrid():
+    game = Game()
+    game.decreaseGrid()
+    game.increaseGrid()
+    assert  game.grid_size == 10
+
